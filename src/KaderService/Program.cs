202f3d7c6594215630d85e00a51665d297790ea7
Aviv Miranda <Aviv943@gmail.com>
2021-03-16 11:31:48 +0200

    ready

--- src/KaderService/Program.cs
+++ src/KaderService/Program.cs
@@ -38,6 +38,7 @@ namespace KaderService
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://0.0.0.0:5000", "http://::5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
